2f16313e91d5596c0b9d4eb95b2f8f9471c99c9d
vladislavzujkov <velocrosss@gmail.com>
2022-09-14 22:18:54 +0300

    #2: feat: добавлены классы для обработки запросов от яндекс диалогов сделан шаблон для обработки запросов диалогов поправил по линтеру

--- src/voice_assistant/domain/assistant/alice/services.py
+++ src/voice_assistant/domain/assistant/alice/services.py
@@ -7,25 +7,31 @@ class AliceService:
     def process_request(self, context: AliceRequest):
         """
         Обработчик запросов с яндекс диалогов.
+
         Args:
             context: AliceRequest
         Returns: AliceResponse
         """
-        answer = 'Hi! I`m bot'
+        answer = "Hi! I`m bot"
         if context.request.nlu.tokens:
             match context.request.nlu.intents.intent_name:
-                case 'search_by_director':
-                    answer = f'search_by_director {context.request.nlu.intents.search_by_director.get("slots").get("person_name").get("value")}'
-                case 'actors_in_the_film':
-                    answer = f'actors_in_the_film {context.request.nlu.intents.actors_in_the_film.get("slots").get("movie_name").get("value")}'
-                case 'film_director':
-                    answer = f'film_director {context.request.nlu.intents.film_director.get("slots").get("movie_name").get("value")}'
-                case 'movie_duration':
-                    answer = f'movie_duration {context.request.nlu.intents.movie_duration.get("slots").get("movie_name").get("value")}'
-                case 'find_film':
-                    answer = f'find_film {context.request.nlu.intents.find_film.get("slots").get("movie_name").get("value")}'
-                case 'empty':
-                    answer = 'empty'
+                case "search_by_director":
+                    value = context.request.nlu.intents.search_by_director.get("slots").get("person_name").get("value")
+                    answer = f"search_by_director {value}"
+                case "actors_in_the_film":
+                    value = context.request.nlu.intents.actors_in_the_film.get("slots").get("movie_name").get("value")
+                    answer = f"actors_in_the_film {value}"
+                case "film_director":
+                    value = context.request.nlu.intents.film_director.get("slots").get("movie_name").get("value")
+                    answer = f"film_director {value}"
+                case "movie_duration":
+                    value = context.request.nlu.intents.movie_duration.get("slots").get("movie_name").get("value")
+                    answer = f"movie_duration {value}"
+                case "find_film":
+                    value = context.request.nlu.intents.find_film.get("slots").get("movie_name").get("value")
+                    answer = f"find_film {value}"
+                case "empty":
+                    answer = "empty"
 
         return AliceResponse(
             meta=context.meta,
@@ -33,6 +39,6 @@ class AliceService:
             version=context.version,
             response={
                 "text": answer,
-                "end_session": False
-            }
+                "end_session": False,
+            },
         )
