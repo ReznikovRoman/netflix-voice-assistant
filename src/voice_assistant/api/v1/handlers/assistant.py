6250d98a200645815f3619e1b3c96c3a91236c81
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-09-15 20:13:31 +0300

    #2: refactor: use FastAPI `Body` instead of manually retrieving request body

--- src/voice_assistant/api/v1/handlers/assistant.py
+++ src/voice_assistant/api/v1/handlers/assistant.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 from dependency_injector.wiring import Provide, inject
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Body, Depends
 
 from voice_assistant.containers import Container
 from voice_assistant.domain.assistant import AssistantProviderDispatcher
@@ -16,10 +16,9 @@ router = APIRouter(
 @router.post("/requests/process", summary="Webhook для провайдера", status_code=HTTPStatus.OK)
 @inject
 async def process_user_request(
-    request: Request,
-    provider: AssistantProviderSlug, *,
+    provider: AssistantProviderSlug,
+    payload: dict = Body(..., description="Данные от провайдера"), *,
     provider_dispatcher: AssistantProviderDispatcher = Depends(Provide[Container.assistant_dispatcher]),
 ):
     """Обработка запроса от пользователя."""
-    provider_data = await request.json()
-    return await provider_dispatcher.dispatch_provider(provider, data=provider_data)
+    return await provider_dispatcher.dispatch_provider(provider, data=payload)
