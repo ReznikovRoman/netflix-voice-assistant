b5713244e5666c767f0d98d2cf199f89ad87abc4
Roman Reznikov <romanreznikov2002@yandex.ru>
2022-09-15 19:59:47 +0300

    #2: chore: add missing type annotations

--- src/voice_assistant/domain/assistant/dispatcher.py
+++ src/voice_assistant/domain/assistant/dispatcher.py
@@ -1,9 +1,9 @@
 from dependency_injector.providers import Factory
 
-from .alice.schemas import AliceRequest
 from .enums import AssistantProviderSlug
 from .exceptions import UnknownAssistantProvider
 from .services import AssistantService, get_assistant_service
+from .types import AssistantResponse
 
 
 class AssistantProviderDispatcher:
@@ -12,12 +12,13 @@ class AssistantProviderDispatcher:
     def __init__(self, assistant_service_factory: Factory[AssistantService]) -> None:
         self._assistant_service_factory = assistant_service_factory
 
-    async def dispatch_provider(self, provider: AssistantProviderSlug, request_body: dict):
+    async def dispatch_provider(self, provider: AssistantProviderSlug, /, *, data: dict) -> AssistantResponse:
         """Выбор корректного сервиса для данного провайдера голосового ассистента."""
         assistant_service = get_assistant_service(self._assistant_service_factory, provider=provider.value)
+        provider_request = assistant_service.build_request_from_provider_data(data)
         match provider:
             case AssistantProviderSlug.YANDEX_ALICE:
-                response = assistant_service.process_request(context=AliceRequest(**request_body))
+                response = assistant_service.process_request(provider_request)
             case _:
                 raise UnknownAssistantProvider()
         return response
